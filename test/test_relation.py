aac160cfcd520fb227acbb45636d925ff8eafceb
Yinlin Chen <ylchen@vt.edu>
2019-03-22 10:57:59 -0400

    replace test_relation.py with test_relationship_to_rdf.py

--- test/test_relation.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from vivotool.utils.relationxmltordf import RelationshipTranslator
-
-
-class TestSample(TestCase):
-    def test_bindAttributes(self):
-        pass
